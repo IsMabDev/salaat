2943f29693725d1960659ba669069ee4bf8c931f
minzo <mabrouk.issam@gmail.com>
2023-09-21 21:01:56 +0100

    update build for gh-pages

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+const JSX = (
+  <div>
+    <h1></h1>
+    <p></p>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+);
+function App() {
+  return JSX;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+//import App from './App';
+//import Bouton from "./boutonToggle";
+//import ControlledInput from "./controlInput";
+//import MyForm from "./controlledForm";
+//import MyApp from "./04-passStateDataToChild";
+//import MyApp from "./05-passCallBackAsProps";
+//import MyComponent from "./06-keydownHandleEvent";
+import MagicEightBall from "./07-javascriptInRenderMethod";
+//import CheckUserAge from "./08-ternarryConditionButons";
+//import GameOfChance from "./09-renderConditionallyFromProps";
+//import MyToDoList from "/home/minzo/Documents/Mes codes/freecodecamp.org/Front End developement libraries/certification projects/10-array.mapToDynamicalyRender.js";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <MagicEightBall />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
